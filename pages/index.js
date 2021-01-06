2a3f72f1f05edb909cb386dbef6d7be0daa50d4e
Dipak Parmar <hi@dipak.tech>
2021-01-05 23:44:06 -0800

    Clean up!

--- pages/index.js
+++ pages/index.js
@@ -5,27 +5,17 @@ import Container from '@/components/container';
 export default function Home({ color }) {
   return (
     <Container>
-      <Flex
-        flexDirection="column"
-        justifyContent="justify-center"
-        alignItems="items-start"
-        maxW="2xl"
-        maxHeight="100vh"
-        mx="auto"
-        mb="16"
+      <Text
+        as="h1"
+        color={color}
+        className="font-bold text-3xl md:text-5xl tracking-tight mb-4 "
       >
-        <Text
-          as="h1"
-          color={color}
-          className="font-bold text-3xl md:text-5xl tracking-tight mb-4 "
-        >
-          Hello 👋
-        </Text>
-        <Text as="h6" color={color} fontSize="2vh" className="mb-16">
-          I'm Dipak Parmar, a Open Source developer 🧑🏻‍💻 currently working at
-          local IT company Adroit Technologies from Kamloops, BC. 🇨🇦
-        </Text>
-      </Flex>
+        Hello 👋
+      </Text>
+      <Text as="h6" color={color} fontSize="2vh" className="mb-16">
+        I'm Dipak Parmar, a Open Source developer 🧑🏻‍💻 currently working at
+        local IT company Adroit Technologies from Kamloops, BC. 🇨🇦
+      </Text>
     </Container>
   );
 }
