77fbe29c439cca7263efeafbfcf1232c0d4292bf
Dipak Parmar <hi@dipak.tech>
2021-01-06 01:58:14 -0800

    Flex version!

--- components/container.js
+++ components/container.js
@@ -1,18 +1,10 @@
-import { useState, useEffect } from 'react';
 import NextLink from 'next/link';
-import { Box, Button, useColorMode, useColorModeValue } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 
 export default function Container({ children }) {
-  const [mounted, setMounted] = useState(false);
-  const { colorMode, toggleColorMode } = useColorMode();
-  useEffect(() => setMounted(true), []);
-
-  const bg = useColorModeValue('white', 'black');
-  const color = useColorModeValue('black', 'white');
-
   return (
     <Box>
-      <Box>{children}</Box>
+      <Box lineHeight="1.5">{children}</Box>
     </Box>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -5,17 +5,50 @@ import Container from '@/components/container';
 export default function Home({ color }) {
   return (
     <Container>
-      <Text
-        as="h1"
-        color={color}
-        className="font-bold text-3xl md:text-5xl tracking-tight mb-4 "
+      <Box
+        as="div"
+        display="flex"
+        height="100vh"
+        flexDirection="column"
+        justifyContent="space-around"
+        mx="0"
+        my="auto"
+        py="120px"
+        px="100px"
+        maxW="1440px"
       >
-        Hello 👋
-      </Text>
-      <Text as="h6" color={color} fontSize="2vh" className="mb-16">
-        I'm Dipak Parmar, a Open Source developer 🧑🏻‍💻 currently working at
-        local IT company Adroit Technologies from Kamloops, BC. 🇨🇦
-      </Text>
+        <Heading
+          as="h1"
+          color={color}
+          fontWeight="300"
+          fontSize="2.5rem"
+          display="block"
+          marginBlock="0.67em"
+          marginInline="0"
+        >
+          Hello 👋
+        </Heading>
+        <Heading
+          as="h2"
+          lineHeight="1.5"
+          color={color}
+          fontWeight="300"
+          maxWidth="700px"
+          fontSize="2.5rem"
+          display="block"
+          marginBlock="0.83em"
+          marginInline="0"
+        >
+          I'm Dipak Parmar, a Open Source developer 🧑🏻‍💻 currently working at
+          local IT company Adroit Technologies from Kamloops, BC. 🇨🇦
+        </Heading>
+        <Heading as="h3" fontSize="1.25rem" color={color} fontWeight="400">
+          Drop a letter at 📧{' '}
+          <Box as="a" href="mailto:hello@dipak.tech">
+            hello@dipak.tech
+          </Box>
+        </Heading>
+      </Box>
     </Container>
   );
 }
