77755959e2c93e54fa672a66b936e7613948c13e
Dipak Parmar <hi@dipak.tech>
2021-06-26 09:53:20 -0700

    Update favicon 🧑🏻‍💻

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,7 @@ export default function App({ Component, pageProps }) {
     <ChakraProvider attribute="class">
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <link rel="icon" href="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/technologist-light-skin-tone_1f9d1-1f3fb-200d-1f4bb.png" type="image/png" />
       </Head>
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
