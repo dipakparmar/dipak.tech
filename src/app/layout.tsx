520727a13f976b7f87f3977c289020e689abcc85
Dipak Parmar <hi@dipak.tech>
2024-06-30 16:10:26 -0700

    fix: remove unused import from next/document

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,10 @@
-import "./globals.css";
+import './globals.css';
 
-import { Head } from "next/document";
-import { Inter } from "next/font/google";
-import type { Metadata } from "next";
-import Script from "next/script";
+import { Inter } from 'next/font/google';
+import type { Metadata } from 'next';
+import Script from 'next/script';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 const title = 'Dipak Parmar';
 const description = 'Open Source Developer from Kamloops, BC 🇨🇦';
@@ -14,43 +13,43 @@ export const metadata: Metadata = {
   title: title,
   description: description,
   openGraph: {
-    type: "website",
-    locale: "en_CA",
-    url: "https://dipak.tech",
+    type: 'website',
+    locale: 'en_CA',
+    url: 'https://dipak.tech',
     siteName: title,
     description: description,
     images: [
       {
-        url: "https://dipak.tech/static/images/banner.jpg",
+        url: 'https://dipak.tech/static/images/banner.jpg',
         width: 1200,
         height: 720,
-        alt: title,
-      },
-    ],
+        alt: title
+      }
+    ]
   },
   twitter: {
-    creator: "@iamdipakparmar",
-    site: "@iamdipakparmar",
-    card: "summary_large_image",
+    creator: '@iamdipakparmar',
+    site: '@iamdipakparmar',
+    card: 'summary_large_image',
     title: title,
-    description: description,
-  },
+    description: description
+  }
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <Script
-				strategy="lazyOnload"
-				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TAG_ID}`}
-			/>
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TAG_ID}`}
+      />
 
-			<Script id="google-analytics" strategy="lazyOnload">
-				{`
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
@@ -58,7 +57,7 @@ export default function RootLayout({
               page_path: window.location.pathname,
             });
                 `}
-			</Script>
+      </Script>
       <body className={inter.className}>{children}</body>
     </html>
   );
