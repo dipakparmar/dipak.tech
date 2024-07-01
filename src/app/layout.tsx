8a02eb0eee07d2ca5910e6c19a26c3254c79d026
Dipak Parmar <hi@dipak.tech>
2024-07-01 11:35:24 -0700

    feat: add intro, about and contact

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,17 @@
 import './globals.css';
 
-import { Inter } from 'next/font/google';
+import { Inter as FontSans } from 'next/font/google';
 import type { Metadata } from 'next';
+import Navbar from '@/components/navbar';
 import Script from 'next/script';
+import { ThemeProvider } from '@/components/theme-provider';
+import { TooltipProvider } from '@/components/ui/tooltip';
+import { cn } from '@/lib/utils';
 
-const inter = Inter({ subsets: ['latin'] });
+const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans'
+});
 
 const title = 'Dipak Parmar';
 const description = 'Open Source Developer from Kamloops, BC 🇨🇦';
@@ -58,7 +65,18 @@ export default function RootLayout({
             });
                 `}
       </Script>
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          'min-h-screen bg-background font-sans antialiased max-w-2xl mx-auto py-12 sm:py-24 px-6',
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <TooltipProvider delayDuration={0}>
+            {children} <Navbar />
+          </TooltipProvider>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,85 @@
-import Image from 'next/image';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+
+import BlurFade from '@/components/magicui/blur-fade';
+import BlurFadeText from '@/components/magicui/blur-fade-text';
+import { DATA } from '@/data/data';
 import Link from 'next/link';
+import Markdown from 'react-markdown';
+
+const BLUR_FADE_DELAY = 0.04;
 
 export default function Home() {
   return (
-    <div className="flex flex-col p-0 m-0 min-h-screen leading-6">
-      <div className="px-10 pt-40 pb-8 my-0 mx-auto w-150 ">
-        <h2 className="block sm:text-lg md:text-2xl lg:text-2xl p-0 mx-0 mt-0 mb-2 text-2xl">
-          Hello <span>👋</span>{' '}
-        </h2>
-        <h2 className="sm:text-lg md:text-2xl lg:text-2xl mt-10 text-2xl">
-          I&apos;m Dipak Parmar, a DevOps Engineer & Open Source developer 🧑🏻‍💻
-        </h2>
-        <h2 className="sm:text-lg md:text-2xl lg:text-2xl mt-10 bottom-5 text-2xl">
-          Drop a letter at{' '}
-          <Link href="mailto:hello@dipak.tech"> 📧 hello@dipak.tech</Link>
-        </h2>
-      </div>
-    </div>
+    <main className="flex flex-col min-h-[100dvh] space-y-10">
+      <section id="hero">
+        <div className="mx-auto w-full max-w-2xl space-y-8">
+          <div className="gap-2 flex justify-between">
+            <div className="flex-col flex flex-1 space-y-1.5">
+              <BlurFadeText
+                delay={BLUR_FADE_DELAY}
+                className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none"
+                yOffset={8}
+                text={`Hi, I'm ${DATA.name.split(' ')[0]} 👋`}
+              />
+              <BlurFadeText
+                className="max-w-[600px] md:text-xl"
+                delay={BLUR_FADE_DELAY}
+                text={DATA.description}
+              />
+            </div>
+            <BlurFade delay={BLUR_FADE_DELAY}>
+              <Avatar className="size-28 border">
+                <AvatarImage alt={DATA.name} src={DATA.avatarUrl} />
+                <AvatarFallback>{DATA.initials}</AvatarFallback>
+              </Avatar>
+            </BlurFade>
+          </div>
+        </div>
+      </section>
+      <section id="about">
+        <BlurFade delay={BLUR_FADE_DELAY * 3}>
+          <h2 className="text-xl font-bold">About</h2>
+        </BlurFade>
+        <BlurFade delay={BLUR_FADE_DELAY * 4}>
+          <Markdown className="prose max-w-full text-pretty font-sans text-sm text-muted-foreground dark:prose-invert">
+            {DATA.summary}
+          </Markdown>
+        </BlurFade>
+      </section>
+      <section id="contact">
+        <div className="grid items-center justify-center gap-2 px-4 text-center md:px-6 w-full py-12">
+          <BlurFade delay={BLUR_FADE_DELAY * 16}>
+            <div className="space-y-3">
+              <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+                Contact
+              </div>
+              <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                Get in Touch
+              </h2>
+              <p className="mx-auto max-w-[600px] text-muted-foreground md:text-sm/relaxed lg:text-sm/relaxed xl:text-sm/relaxed">
+                Interested in connecting? Feel free to reach out with a direct
+                question via {' '}
+                <Link
+                  href={DATA.contact.social.X.url}
+                  className="text-blue-500 hover:underline"
+                >
+                  Twitter
+                </Link>{' '}
+                or{' '}
+                <Link
+                  href={DATA.contact.social.LinkedIn.url}
+                  className="text-blue-500 hover:underline"
+                >
+                  Linkedin
+                </Link>
+                . I&apos;m always open to new opportunities and collaborations,
+                and I&apos;ll do my best to respond promptly. Please note that I
+                do not engage with unsolicited offers or advertisements.
+              </p>
+            </div>
+          </BlurFade>
+        </div>
+      </section>
+    </main>
   );
 }
