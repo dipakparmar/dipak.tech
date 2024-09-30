b39919db47b7d063848287e708c108a3492d8afe
Dipak Parmar <hi@dipak.tech>
2024-09-29 20:23:37 -0700

    feat(security): add security headers and Content Security Policy to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,62 @@
+const ContentSecurityPolicy = `
+	default-src 'self';
+	script-src 'self' https://www.googletagmanager.com 'unsafe-inline' 'unsafe-eval';
+	style-src 'self' 'unsafe-inline';
+	child-src ;
+	frame-src ;
+	connect-src 'self';
+	img-src 'self' https://github.com https://avatars.githubusercontent.com data:;
+	media-src 'self';
+	font-src 'self';
+`;
+
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on'
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload'
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=()'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin-when-cross-origin'
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+  }
+];
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
+  reactStrictMode: true,
+
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders
+      }
+    ];
+  }
 };
 
 export default nextConfig;
