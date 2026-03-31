ce89d17c50beddbe62d3033cbb1437a1823268aa
Dipak Parmar <hi@dipak.tech>
2026-03-31 04:42:39 -0700

    feat(blog): add RSS 2.0 feed

---
+++ src/app/(portfolio-site)/blog/rss.xml/route.ts
@@ -0,0 +1,39 @@
+import { getAllPosts } from '@/lib/blog';
+import { DATA } from '@/data/data';
+
+export async function GET() {
+  const posts = getAllPosts();
+
+  const items = posts
+    .map(
+      (post) => `
+    <item>
+      <title><![CDATA[${post.title}]]></title>
+      <link>https://dipak.tech/blog/${post.slug}</link>
+      <guid isPermaLink="true">https://dipak.tech/blog/${post.slug}</guid>
+      <description><![CDATA[${post.description}]]></description>
+      <pubDate>${new Date(post.date).toUTCString()}</pubDate>
+      ${post.tags.map((tag) => `<category>${tag}</category>`).join('\n      ')}
+    </item>`
+    )
+    .join('');
+
+  const feed = `<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+  <channel>
+    <title>${DATA.name} - Blog</title>
+    <link>https://dipak.tech/blog</link>
+    <description>Writings on software engineering, infrastructure, and DevSecOps.</description>
+    <language>en-us</language>
+    <lastBuildDate>${posts.length > 0 ? new Date(posts[0].date).toUTCString() : new Date().toUTCString()}</lastBuildDate>
+    <atom:link href="https://dipak.tech/blog/rss.xml" rel="self" type="application/rss+xml" />${items}
+  </channel>
+</rss>`;
+
+  return new Response(feed, {
+    headers: {
+      'Content-Type': 'application/rss+xml; charset=utf-8',
+      'Cache-Control': 'public, max-age=3600, s-maxage=3600',
+    },
+  });
+}
